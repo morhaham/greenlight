8a63ba6b665f25a53f6bc4409dbb8971659f387c
morhaham2 <morh@codeoasis.com>
2024-10-05 18:07:17 +0300

    sort movies done

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -5,13 +5,12 @@ import (
 	"net/http"
 )
 
-func (app *application) logError(r *http.Request, err error) {
+func (app *application) logError(_ *http.Request, err error) {
 	app.logger.Println(err)
 }
 
 func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, status int, message interface{}) {
 	env := envelope{"error": message}
-
 	err := app.writeJSON(w, status, env, nil)
 	if err != nil {
 		app.logError(r, err)
@@ -21,7 +20,6 @@ func (app *application) errorResponse(w http.ResponseWriter, r *http.Request, st
 
 func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
 	app.logError(r, err)
-
 	message := "the server encountered a problem and could not process your request"
 	app.errorResponse(w, r, http.StatusInternalServerError, message)
 }
