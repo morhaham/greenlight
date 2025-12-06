dcfaaa7d7cc1fc1752e69fea6e66f176436f7a24
morhaham <you@example.com>
2025-12-06 23:21:13 +0200

    handle preflight requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -172,13 +172,21 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Vary", "Origin")
+		w.Header().Add("Vary", "Access-Control-Request-Method")
 		origin := r.Header.Get("Origin")
 		trustedOrigins := app.config.cors.trustedOrigins
 		if len(trustedOrigins) > 0 {
 			for _, trustedOrigin := range trustedOrigins {
 				if strings.EqualFold(origin, trustedOrigin) {
 					w.Header().Set("Access-Control-Allow-Origin", origin)
-					break
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
 				}
 			}
 		}
